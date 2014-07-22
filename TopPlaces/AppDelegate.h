1ef6f7bc65763f40fdb1e2793ad81a59e1d2e0ea
ruthwickp <ruthwickp@yahoo.com>
2014-07-22 09:32:21 -0700

    Added Storyboard interface

---
+++ TopPlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Ruthwick Pathireddy on 7/21/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopPlaces
+//
+//  Created by Ruthwick Pathireddy on 7/21/14.
+//  Copyright (c) 2014 Darkking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
